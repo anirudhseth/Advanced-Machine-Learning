c080b797502ddc23b956403976bd3e69f149b52f
anirudh seth <anirudhseth14@gmail.com>
2019-11-23 20:47:04 +0100

    lab1 q9 done

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -21,19 +21,20 @@ def plot2dGaussian(mu,cov,w0,w1,title):
 	plt.show()
 
 def calPosterior(prior_mu,prior_cov,noise_mu,noise_cov,X_Label,T,points):
-	temp=random.randint(0,X_Label.shape[0])
+	
 	X_observed=np.ones(points)
 	T_observed=np.ones(points)
 	for i in range(points):
-		temp=random.randint(0,X_Label.shape[0])
+		temp=random.randint(0,X_Label.shape[0]-1)
 		X_observed[i]=X_Label[temp,1]
 		T_observed[i]=T[temp,0]
+		# print(temp)
 	X_observed=X_observed.reshape(-1,1)
 	X_observed=np.hstack((X_observed,np.ones(X_observed.shape[0]).reshape(-1,1)))
 	X_observed=np.flip(X_observed,axis=1)
 	# print(X_observed)
 	T_observed=T_observed.reshape(-1,1)
-	print(T_observed)
+	# print(T_observed)
 	posterior_cov=np.linalg.inv((1./(noise_cov))*(np.matmul(X_observed.transpose(),X_observed))+np.linalg.inv(prior_cov))
 	posterior_mean=np.matmul(posterior_cov,np.matmul(X_observed.transpose(), T_observed))/noise_cov
 	posterior_mean=posterior_mean.flatten()
@@ -54,33 +55,36 @@ T=np.matmul(X_Label,W)+noise
 
 # plot prior
 prior_mu=np.array([0,0])
-prior_cov=np.array([[0.25,0],[0,0.25]])
+prior_cov=np.array([[0.1,0],[0,0.1]])
 w0=np.arange(-2,2,0.1)
 w1=np.arange(-2,2,0.1)
 title='P(w):Prior Distribution over W'
 plot2dGaussian(prior_mu,prior_cov,w0,w1,title)
 
-# compute posterior for 1 data point
-posterior_mean,posterior_cov=calPosterior(prior_mu,prior_cov,noise_mu,noise_cov,X_Label,T,100)
-w0=np.arange(-2,2,0.1)
-w1=np.arange(-2,2,0.1)
-title='Poseterior after Observing one point'
-plot2dGaussian(posterior_mean,posterior_cov,w0,w1,title)
-
-# draw 5 samples from posterior and plot function
-
-for i in range(5):
-	w_samples = np.random.multivariate_normal(posterior_mean, posterior_cov, 5)
-print('----------------')	
-print(w_samples)
-print('----------------')
-p=np.arange(-1,1.01,.01)
-plt.plot(p, 0.5*p-1.5)	
-for i in range(5):
-	# Plot line based on sampled parameters
-	plt.plot(p, w_samples[i][1]*p + w_samples[i][0], color = "red", linestyle = "-", linewidth = 0.5)
-
-plt.show()
+# compute posterior for n data point
+N=[1,3,5,7,50,100]
+for n in N:
+	posterior_mean,posterior_cov=calPosterior(prior_mu,prior_cov,noise_mu,noise_cov,X_Label,T,n)
+	w0=np.arange(-2,2,0.1)
+	w1=np.arange(-2,2,0.1)
+	title="Posterior Distribution after Observing",n,"point(s)"
+	plot2dGaussian(posterior_mean,posterior_cov,w0,w1,title)
+	# draw 5 samples from posterior and plot function
+	for i in range(5):
+		w_samples = np.random.multivariate_normal(posterior_mean, posterior_cov, 5)
+	# print(w_samples)
+	p=np.arange(-2,2,.5)
+	plt.plot(p, 0.5*p-1.5,label="True Function")	
+	for i in range(5):
+		# Plot line based on sampled parameters
+		plt.plot(p, w_samples[i][1]*p + w_samples[i][0], color = "red", linestyle = "-", linewidth = 0.5)
+		plt.xlabel('x')
+		plt.ylabel('t')
+		# plt.axis('scaled')
+		# plt.axis('square')
+	plt.legend()
+	plt.title('Samples drawn from Posterior')
+	plt.show()
 
 # # compute posterior
 # posterior_mean,posterior_cov=calPosterior(prior_mu,prior_cov,noise_mu,noise_cov,X_Label,T,100)
