1396b1f27b3c9e1f5730b792675199a4e0e023e0
anirudh seth <anirudhseth14@gmail.com>
2019-11-24 19:31:32 +0100

    q11 done

--- Lab1/gp.py
+++ Lab1/gp.py
@@ -1,24 +1,80 @@
 import pylab as pb
 import numpy as np
 from math import pi
+import scipy
 from scipy.spatial.distance import  cdist
 from scipy.stats import multivariate_normal
 import matplotlib.pyplot as plt
 import random
 
+def sqExpKernel(X,Y,length_scale):
+	return np.exp(-cdist(X, Y, 'sqeuclidean')/(length_scale**2))
+
+def data():
+	X=np.array([-2,-1.5,-1,-0.5,0,0.5,1,1.5,2]).reshape(-1,1)
+	# X=np.array([-2,-1.5,-1,-0.5,0,0.5,1,1.5,2])
+	X=X*pi
+	noise = np.random.normal(0, 0.2, X.shape[0]).reshape(-1,1)
+	# noise = np.random.normal(0, 0.2, X.shape[0])
+	# T=np.sin(X)+np.cos(X)+noise
+	T=np.cos(X)
+	return X,T
+	
 def plotGP(noise,length_scale,no_of_samples):
 	X = np.linspace(-10.0,10.0,100) 
 	X=X.reshape(-1,1)
 	mu = np.zeros(X.shape[0])
-	K =  noise*np.exp(-cdist(X, X, 'sqeuclidean')/(length_scale**2))
+	K =  noise*sqExpKernel(X,X,length_scale)
 	Z = np.random.multivariate_normal(mu,K,no_of_samples)
+	
+	
 	for i in range(no_of_samples):
 		plt.plot(X[:],Z[i,:])
-	plt.title('Gaussin Prior with length-scale:'+str(length_scale))
+	plt.title('Length Scale:'+str(length_scale))
+	
+	# plt.savefig('q10/q10_GP_'+str(length_scale)+'.png')
 	plt.show()
 
-noise=1
-length_scale=[0.1,1,10,100]
-no_of_samples=10
-for l in  length_scale:
-	plotGP(noise,l,no_of_samples)
+# https://peterroelants.github.io/posts/gaussian-process-tutorial/
+def GP_noise(X, T, X_star, σ_noise):
+	length_scale=2
+	# Kernel of the noisy observations
+	Σ11 = exponentiated_quadratic(X, X,length_scale) + σ_noise * np.eye(X.shape[0])
+	# Kernel of observations vs to-predict
+	Σ12 = exponentiated_quadratic(X, X_star,length_scale)
+	# Solve
+	solved = scipy.linalg.solve(Σ11, Σ12, assume_a='pos').T
+	# Compute posterior mean
+	μ2 = solved @ T
+	# Compute the posterior covariance
+	Σ22 = exponentiated_quadratic(X_star, X_star,length_scale)
+	Σ2 = Σ22 - (solved @ Σ12)
+	return μ2, Σ2  # mean, covariance
+
+
+# # plot gp
+# for l in  length_scale:
+# 	plotGP(noise,l,no_of_samples)
+
+X,T=data()
+# X_star=np.array([[-4.28318531]])
+X_star = np.linspace(-3*np.pi, 3*np.pi, 800).reshape(-1,1)
+pos_mean,pos_cov=GP_noise(X,T,X_star,0)
+# pos_mean,pos_cov=computePosterior(X_star,X,T,1)
+op=np.random.multivariate_normal(mean=pos_mean.flatten(), cov=pos_cov, size=9)
+# plt.plot(X[:],op[:])
+# print(pos_cov)
+# print(pos_mean)
+plt.plot(X, T,'ro')
+plt.plot(X_star,np.cos(X_star), color = 'green')
+plt.plot(X_star,pos_mean, color = 'blue',linewidth=0.8)
+Z = np.random.multivariate_normal(np.reshape(pos_mean,(800,)),pos_cov,10)
+for i in range(10):
+	pb.plot(X_star[:],Z[i,:],linewidth=0.4)
+# print(pos_cov.diagonal())
+pos_mean=pos_mean.flatten()
+upper = pos_mean + 2*np.sqrt(pos_cov.diagonal())
+lower = pos_mean - 2*np.sqrt(pos_cov.diagonal())
+ax = plt.gca()
+# print(pos_cov.diagonal().shape)
+ax.fill_between(np.linspace(-3*np.pi, 3*np.pi, 800), upper, lower, facecolor='yellow', interpolate=True, alpha=0.1)
